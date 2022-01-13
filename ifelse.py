89f67b3b64a7cab59167b906c6d9b094c405160a
nvluat-hcmut <nguyenvanluat302@gmail.com>
2022-01-13 11:05:35 +0700

    try commit -a

--- ifelse.py
+++ ifelse.py
@@ -3,3 +3,13 @@ if (a%3==0):
     print("Divisible by 3")
 else:
     print("Not divisible by 3")
+
+a = 'hai'
+dic = {
+    'mot': 1,
+    'hai': 2,
+    'ba': 3,
+}
+print(dic.get(a,'khong ro')) # get() method, if not have a in dic, will print "khong ro"
+
+# Ket Qua: 2
