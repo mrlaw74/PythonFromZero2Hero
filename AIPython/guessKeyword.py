7f0858c8d2f2eed00d40a70d5a3fe6368c129b52
nvluat-hcmut <nguyenvanluat302@gmail.com>
2022-06-19 23:12:37 +0700

    Update GuessKeyword --- done

--- AIPython/guessKeyword.py
+++ AIPython/guessKeyword.py
@@ -8,18 +8,17 @@ heart = "\u2764\uFE0F "
 answer = random.choice(string_random)
 print(answer)
 LenghtOfAnswer = len(answer)
-print(LenghtOfAnswer)
+check = 0
+# print(LenghtOfAnswer)
 
 def InitialAnswerArray(n):
     arr_string = ''
-    # arr_string += '['
     print('[', end='')
     for index in range(LenghtOfAnswer):
         if index < (LenghtOfAnswer - 1):
             arr_string += '\'?\','
         else:
             arr_string += '\'?\''
-    # arr_string += ']'
     print(arr_string, end='')
     print(']')
     print("Lives left: " + 9*heart)
@@ -27,37 +26,45 @@ def InitialAnswerArray(n):
 array_string = InitialAnswerArray(LenghtOfAnswer)
 
 def checkWord(word, result):
-    index_tmp = 0
+    global check
+    index_tmp = []
     flag = False
     if word == result:
         print('Bingo, You win!!!')
+        quit()
     for index in range(LenghtOfAnswer):
         if word == result[index]:
-            # index_tmp.append(index)
-            index_tmp = index
-            # result[index_tmp] = None
+            index_tmp.append(index)
             flag = True
+    check += len(index_tmp)
     return index_tmp, flag
 
+def checkWin():
+    global check
+    global LenghtOfAnswer
+    if check == LenghtOfAnswer:
+        print('Bingo, You win!!!')
+        quit()
+    
 def in_game():
-    # global lives_remain
-    # print(checkWord(guessword, answer))
-    # print(array_string)
-    # print(array_string.split(','))
+    global lives_remain
     list_arr = array_string.split(',')
-    # list_arr = list(array_string)
-    # print(list_arr)
     flag = True
-    while(flag):
+    while(lives_remain != 0):
+        checkWin()
         guessword = input(str('You guess the word is: '))
         ind_, flag = checkWord(guessword, answer)
         if flag:
-            # list_arr.index = guessword
-            # list_arr = list(map(lambda x: x.replace(array_string[ind_], guessword), array_string))
-            list_arr[ind_] = guessword
-            pass
+            for index in ind_:
+                list_arr[index] = guessword
+            print("Good job bro!")
         else:
-            pass
+            lives_remain -= 1
+            if (lives_remain > 0):
+                print("Lives left: " + lives_remain*heart)
+            else:
+                print ("Game over")
+                quit()
         print(list_arr)
 
 in_game()
